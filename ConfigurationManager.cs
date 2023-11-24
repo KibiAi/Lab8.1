b87b65899af4306c99d58356452eaa2baf93da2d
KibiAi <vetal2004v@gmail.com>
2023-11-24 12:33:39 +0200

    Добавьте файлы проекта.

---
+++ ConfigurationManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8._1
+{
+    public sealed class ConfigurationManager
+    {
+        private static ConfigurationManager instance = null;
+        private static readonly object padlock = new object();
+
+        public Dictionary<string, string> Settings { get; private set; }
+
+        ConfigurationManager()
+        {
+            Settings = new Dictionary<string, string>();
+        }
+
+        public static ConfigurationManager Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new ConfigurationManager();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void UpdateSetting(string key, string value)
+        {
+            if (Settings.ContainsKey(key))
+            {
+                Settings[key] = value;
+            }
+            else
+            {
+                Settings.Add(key, value);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Lab8._1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Введіть ключ налаштування:");
+            string key = Console.ReadLine();
+            Console.WriteLine("Введіть значення налаштування:");
+            string value = Console.ReadLine();
+
+            ConfigurationManager.Instance.UpdateSetting(key, value);
+
+            Console.WriteLine($"Нове значення для {key} встановлено на {ConfigurationManager.Instance.Settings[key]}");
+        }
+    }
+}
